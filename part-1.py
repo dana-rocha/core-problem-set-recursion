0f7fd9ec391945a6c257d3a12144df2dfdabf936
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-05-08 23:31:38 -0700

    Problems 1 and 3 complete

--- part-1.py
+++ part-1.py
@@ -4,7 +4,13 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    elif n <= 1:
+        return 1
 
+    return n * (factorial(n - 1))
 
 
 # reverse
@@ -12,7 +18,15 @@
 
 
 # bunny
+def bunny(count):
 
+    if count < 0:
+        raise ValueError
+    
+    elif count == 0:
+        return 0
+    
+    return 2 + bunny(count - 1)
 
 
 # is_nested_parens
